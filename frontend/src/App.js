6222465b3bf230acfdb189265b28b1ac14cdaec5
naszvett <110763865+naszvett@users.noreply.github.com>
2023-01-20 11:53:32 +0100

    react app created

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import { storeName } from './constants';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">{storeName}</a>
+      </header>
+      <main>temp</main>
+    </div>
+  );
+}
+
+export default App;
