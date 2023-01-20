0af3fa1335af1d5537950c7ae391529a5978e237
naszvett <110763865+naszvett@users.noreply.github.com>
2023-01-20 12:30:05 +0100

    list products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { storeName } from './constants';
+import data from './data';
 
 function App() {
   return (
@@ -6,7 +7,27 @@ function App() {
       <header className="App-header">
         <a href="/">{storeName}</a>
       </header>
-      <main>temp</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
